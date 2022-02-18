840c40a21d2d28645a7be210c5563fe457a82679
iheboueslati <58653492+iheboueslati@users.noreply.github.com>
2022-02-18 16:14:39 +0100

    added controller

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var  cors = require('cors');
 var corsOptions = {
   origin: "http://localhost:8081"
 };
-
+const db = require("./models");
 //bezkoder : listen for requests
 //const PORT = process.env.PORT || 8080 ;
 
@@ -40,8 +40,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/os',osRouter);
 app.use('/products',productsRouter);
-
-//bezkoder : simple route
+ 
+//bezkoder : simple route jj
 app.get('/' , (req , res)=>{
 
    res.json({message: "welcome to my personal app"});
@@ -68,5 +68,20 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
  
+//bezkoder
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+    console.log("Port  "+process . env.PORT);
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 
 module.exports = app;
---
+++ controllers/tutorial.controller.js
@@ -0,0 +1,43 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+
+//create and save a new tutorial
+exports.create = (req, res) => {
+  //      validate request
+  if (!req.body.title) {
+    res.status(400).send({ message: "Content cannot be empty!" });
+  }
+  //      Create
+  const tutorial = new Tutorial({
+    title: req.body.title,
+    description: req.body.description,
+    published: req.body.published ? req.body.published : false,
+  });
+  //save the tutorial
+  tutorial
+    .save(tutorial)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: err.message || "Some error has occured" });
+    });
+};
+
+//retrieve all the tutorials
+exports.findAll = (req, res) => {};
+
+//Find a single tutorial by its ID
+exports.findOne = (req, res) => {};
+
+//update a tutorial by its in the request
+exports.update = (req, res) => {};
+
+// Delete a Tutorial with the specified id in the request
+exports.delete = (req, res) => {};
+// Delete all Tutorials from the database.
+exports.deleteAll = (req, res) => {};
+// Find all published Tutorials
+exports.findAllPublished = (req, res) => {};
